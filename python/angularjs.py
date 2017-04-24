e13ffd2e8879e6946f754fb4adfc6fe50357c16b
Tuyen <tuyen@Mac-mini.local>
2017-04-24 18:21:23 +0700

    python selenium

--- python/angularjs.py
+++ python/angularjs.py
@@ -2,16 +2,20 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 
-#setting username and password
+
+#setting available
 usr = "admin@example.com"
 pwd = "secret"
+archer_number = "Selenium Test"
 
 #setup webdriver
-delay = 15
+delay = 30
+sleep = 10
 driver = webdriver.Firefox()
 driver.get("http://localhost:8000/#/")
 assert "Remediation Portal" in driver.title
@@ -24,27 +28,29 @@ try:
     elem = driver.find_element_by_id("passwd")
     elem.send_keys(pwd)
     elem.send_keys(Keys.RETURN)
-    element = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.ID, "u-logout")))
+    WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.ID, "u-logout")))
     assert "incidents" in driver.current_url
-    WebDriverWait(driver, delay)
 
     #select incident and click on View
-    driver.find_element_by_css_selector(".incidient-table tr td>input:first-child").click()
-    WebDriverWait(driver, delay)
-    driver.find_element_by_css_selector(".selected-highlight").click()
-    WebDriverWait(driver, delay)
+    WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.ID, "check-incident-0"))).click()
+    WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.CLASS_NAME, "selected-highlight"))).click()
 
     #process to assessment form
-    driver.find_element_by_css_selector(".btn-proceed").click()
-    WebDriverWait(driver, delay)
-    driver.find_element_by_css_selector(".confirm-process").click()
+    WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.CLASS_NAME, "btn-proceed"))).click()
+    WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.CLASS_NAME, "confirm-process"))).click()
 
     #process to create assessment
+    elem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.ID, "archer-number")))
+    elem.send_keys(archer_number)
+    driver.find_element_by_css_selector("button[ng-click='vm.clickAddStakeholders()']").click()
+    selectStakeholder = Select(driver.find_element_by_css_selector("select[ng-model='modalValue.stakeholderValue']"))
+    selectStakeholder.select_by_index(1)
+
 
 except Exception, e:
     print 'Exception: ' + str(e)
 
 finally:
-    print "Finally!"
-    time.sleep(5)
+    print "OK"
+    time.sleep(sleep)
     driver.quit()
